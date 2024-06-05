5b5d56d36da7fafbdd3f5b9d7bb706ae3e80c6ea
GregorisB <gregorbachts@gmail.com>
2024-06-06 00:07:40 +0300

    delete vite directory

--- vite project setup/create_directory_structure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Create directory structure in src
-echo "Creating directory structure in src"
-mkdir -p src/{app,assets,components,constants,features,hooks,layouts,routes,utils}
-mkdir -p src/app/services
-
-echo "Directory structure created successfully."
