0488b50ef869399a223d25f26a22007303768948
Gregoris Bachtsevanos <94171068+gregorisbachtsevanos@users.noreply.github.com>
2024-07-03 14:39:55 +0300

    Update organize_files.py

--- organize files/organize_files.py
+++ organize files/organize_files.py
@@ -1,23 +1,35 @@
 import os
 import shutil
 
-def organize_files(directory):
-    if not os.path.exists(directory):
-        print(f"The directory '{directory}' does not exist.")
-        return
+# Define the source directory and the target directories
+source_dir = '/path/to/source'
+image_dir = '/path/to/Photos'
+document_dir = '/path/to/Documents'
+music_dir = '/path/to/Music'
 
-    for filename in os.listdir(directory):
-        if os.path.isfile(os.path.join(directory, filename)):
-            file_extension = filename.split('.')[-1]
-            folder_name = os.path.join(directory, file_extension.upper())
+# Create target directories if they don't exist
+os.makedirs(image_dir, exist_ok=True)
+os.makedirs(document_dir, exist_ok=True)
+os.makedirs(music_dir, exist_ok=True)
 
-            if not os.path.exists(folder_name):
-                os.makedirs(folder_name)
+# Define file type associations
+file_types = {
+    'Images': ['.jpg', '.jpeg', '.png', '.gif'],
+    'Documents': ['.pdf', '.docx', '.txt'],
+    'Music': ['.mp3', '.wav']
+}
 
-            shutil.move(os.path.join(directory, filename), os.path.join(folder_name, filename))
+# Function to move files based on their extension
+def move_files():
+    for filename in os.listdir(source_dir):
+        file_ext = os.path.splitext(filename)[1].lower()
+        src_path = os.path.join(source_dir, filename)
+        
+        if file_ext in file_types['Images']:
+            shutil.move(src_path, os.path.join(image_dir, filename))
+        elif file_ext in file_types['Documents']:
+            shutil.move(src_path, os.path.join(document_dir, filename))
+        elif file_ext in file_types['Music']:
+            shutil.move(src_path, os.path.join(music_dir, filename))
 
-    print("Files have been organized.")
-
-if __name__ == "__main__":
-    directory = input("Enter the path of the directory to organize: ")
-    organize_files(directory)
+move_files()
