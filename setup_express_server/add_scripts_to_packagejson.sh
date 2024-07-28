b65d49e4aba178f06dd561d2f8196819f80c81f0
Gregoris Bachtsevanos <94171068+gregorisbachtsevanos@users.noreply.github.com>
2024-07-28 16:16:46 +0300

    Delete setup_express_server directory

--- setup_express_server/add_scripts_to_packagejson.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Add scripts to package.json
-echo "Adding scripts to package.json"
-npx json -I -f package.json -e 'this.scripts["dev"]="nodemon ./bin/www.js --ignore client"'
-
-echo "Scripts added to package.json successfully."
--- setup_express_server/create_directory_structure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create directory structure in src
-echo "Creating directory structure in src"
-mkdir -p src/{config,constants,controllers,db,middleware,models,routes,utils}
-
-echo "Directory structure created successfully."
